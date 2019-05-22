a6afd61598bee0734aa92814d12cfbea284eaa94
Carlos Huchim Ahumada <huchim@live.com.mx>
2019-05-22 12:32:36 -0500

    cambios

--- configurations/templates/node1.sh
+++ configurations/templates/node1.sh
@@ -7,7 +7,6 @@ mkdir -p ./tmp
 mkdir -p ./configurations
 mkdir -p ./configurations/webpack
 
-
 # Webpack
 npm install -D --no-audit --prefer-offline webpack@^4.31.0  webpack-cli@^3.3.2  webpack-node-externals@^1.7.2
 npm install -D --no-audit --prefer-offline html-webpack-plugin@^3.2.0  mini-css-extract-plugin@^0.4.2  webpack-concat-plugin@^2.4.2
@@ -15,4 +14,12 @@ npm install -D --no-audit --prefer-offline css-loader@^2.1.1 file-loader@^3.0.1 
 
 # Typescript support
 npm install -D --no-audit --prefer-offline typescript@^3.4.5 tslint@^5.16.0 tslint-microsoft-contrib@^6.1.1 typescript-tslint-plugin@^0.3.1
+npm install -D --no-audit --prefer-offline happypack@^5.0.0 ts-loader@^3.5.0 tsconfig-paths-webpack-plugin@^3.2.0
+
+# Mocha supports && coverage
+npm install -D --no-audit --prefer-offline mocha@^5.0.4 @types/mocha@^2.2.48 chai@^4.1.2 @types/chai@^4.1.2
+npm install -D --no-audit --prefer-offline ts-node@^5.0.1 tsconfig-paths@^3.4.2
+
+# Sass Support
+npm install -D --no-audit --prefer-offline sass-loader@^7.1.0
 npm install node-sass --no-save
