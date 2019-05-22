961d42d7bc00210e177e4a2ec257f1226ce4ebf5
Carlos Huchim Ahumada <huchim@live.com.mx>
2019-05-22 12:11:04 -0500

    add typsceript support

--- configurations/templates/node1.sh
+++ configurations/templates/node1.sh
@@ -7,5 +7,12 @@ mkdir -p ./tmp
 mkdir -p ./configurations
 mkdir -p ./configurations/webpack
 
-npm install webpack@^4.31.0  webpack-cli@^3.3.2  webpack-node-externals@^1.7.2  css-loader@^2.1.1  file-loader@^3.0.1  sass-loader@^7.1.0  html-webpack-plugin@^3.2.0  mini-css-extract-plugin@^0.4.2  webpack-concat-plugin@^2.4.2  typescript@^3.4.5  tslint@^5.16.0  tslint-microsoft-contrib@^6.1.1 -D --no-audit --prefer-offline
+
+# Webpack
+npm install -D --no-audit --prefer-offline webpack@^4.31.0  webpack-cli@^3.3.2  webpack-node-externals@^1.7.2
+npm install -D --no-audit --prefer-offline html-webpack-plugin@^3.2.0  mini-css-extract-plugin@^0.4.2  webpack-concat-plugin@^2.4.2
+npm install -D --no-audit --prefer-offline css-loader@^2.1.1 file-loader@^3.0.1 sass-loader@^7.1.0
+
+# Typescript support
+npm install -D --no-audit --prefer-offline typescript@^3.4.5 tslint@^5.16.0 tslint-microsoft-contrib@^6.1.1 typescript-tslint-plugin@^0.3.1
 npm install node-sass --no-save
