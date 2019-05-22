6ed172e719c19359f6a7d87faa08eca34114372d
Carlos Huchim Ahumada <huchim@live.com.mx>
2019-05-22 15:00:57 -0500

    change dependency

--- configurations/templates/node1.sh
+++ configurations/templates/node1.sh
@@ -10,7 +10,7 @@ mkdir -p ./configurations/webpack
 # Webpack
 npm install -D --no-audit --prefer-offline webpack@^4.31.0  webpack-cli@^3.3.2  webpack-node-externals@^1.7.2
 npm install -D --no-audit --prefer-offline html-webpack-plugin@^3.2.0  mini-css-extract-plugin@^0.4.2  webpack-concat-plugin@^2.4.2
-npm install -D --no-audit --prefer-offline css-loader@^2.1.1 file-loader@^3.0.1 sass-loader@^7.1.0
+npm install -D --no-audit --prefer-offline css-loader@^2.1.1 file-loader@^3.0.1 style-loader@^0.20.1
 
 # Typescript support
 npm install -D --no-audit --prefer-offline typescript@^3.4.5 tslint@^5.16.0 tslint-microsoft-contrib@^6.1.1 typescript-tslint-plugin@^0.3.1
